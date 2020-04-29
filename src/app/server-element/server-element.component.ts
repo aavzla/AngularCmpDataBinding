f77019f4edd035bf2661a00a8c4d6da142b37c1f
AAvilan <andres.avilan@nurun.com>
2020-04-29 14:40:14 -0400

    Assigning an Alias to Custom Properties - Assigning an alias to a property in a child component to be accessible outside through the alias in the parent component.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerElementComponent implements OnInit {
   //The curly brackets here are the definition in TypeScript of a JavaScript object.
   //So the property will only accept a JavaScript object.
-  @Input() element: { type: string, name: string, content: string };
+  @Input('srvElement') element: { type: string, name: string, content: string };
 
   constructor() { }
 
