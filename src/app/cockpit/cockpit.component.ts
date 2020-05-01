30159dd62ac049c2c817070bd25d7e8fdb7fe768
AAvilan <andres.avilan@nurun.com>
2020-05-01 13:59:21 -0400

    Getting Access to the Template & DOM with @ViewChild - Use of the decorator ViewChild to have access to the HTML element of the template in the TypeScript file. In this case, we change the server content input local reference sent from a parameter of a method to be accessible through a property of type ElementRef.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,6 +10,7 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated : EventEmitter<{ serverName: string, serverContent: string }>;
   //newServerName = '';
   //newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput : ElementRef;
 
   constructor() {
     this.serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
@@ -19,23 +20,24 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer(serverNameHTMLElement: HTMLInputElement, serverContentHTMLElement: HTMLInputElement) {
+  onAddServer(serverNameHTMLElement: HTMLInputElement) {
     //Log to the console of the parameter received for testing purposes
     //console.log(serverNameHTMLElement.value);
+    //console.log(this.serverContentInput.nativeElement.value);
     this.serverCreated.emit({
       //serverName: this.newServerName,
       //serverContent: this.newServerContent
       serverName: serverNameHTMLElement.value,
-      serverContent: serverContentHTMLElement.value
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint(serverNameHTMLElement: HTMLInputElement, serverContentHTMLElement: HTMLInputElement) {
+  onAddBlueprint(serverNameHTMLElement: HTMLInputElement) {
     this.blueprintCreated.emit({
       //serverName: this.newServerName,
       //serverContent: this.newServerContent
       serverName: serverNameHTMLElement.value,
-      serverContent: serverContentHTMLElement.value
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 }
