560201fe3b79af175321ca63faa659701993db37
AAvilan <andres.avilan@nurun.com>
2020-05-01 13:44:00 -0400

    Using Local References in Templates - Add local references to a component so it can be used in the template or can be passed to methods as parameters. In this case. we added local references to the cockpit component to pass the HTML input elements to the TypeScript methods in order to get the values of the inputs.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated : EventEmitter<{ serverName: string, serverContent: string }>;
   @Output('bpCreated') blueprintCreated : EventEmitter<{ serverName: string, serverContent: string }>;
-  newServerName = '';
-  newServerContent = '';
+  //newServerName = '';
+  //newServerContent = '';
 
   constructor() {
     this.serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
@@ -19,17 +19,23 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(serverNameHTMLElement: HTMLInputElement, serverContentHTMLElement: HTMLInputElement) {
+    //Log to the console of the parameter received for testing purposes
+    //console.log(serverNameHTMLElement.value);
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      //serverName: this.newServerName,
+      //serverContent: this.newServerContent
+      serverName: serverNameHTMLElement.value,
+      serverContent: serverContentHTMLElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameHTMLElement: HTMLInputElement, serverContentHTMLElement: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      //serverName: this.newServerName,
+      //serverContent: this.newServerContent
+      serverName: serverNameHTMLElement.value,
+      serverContent: serverContentHTMLElement.value
     })
   }
 }
