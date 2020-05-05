4e18b62a380147b80d7777964ff902e84651ad4e
AAvilan <andres.avilan@nurun.com>
2020-05-05 08:18:02 -0400

    Seeing Lifecycle Hooks in Action - Lifecycle hooks actions methods allow you to implement a logic though out the  lifecycle of the page. Depending of which action you want to make there is a hook for implementing that scenario. In this case, we just implemented all in the server element component in order to show the order of fire of each lifecycle hook action at the start of the page and also for the actions performed by the user.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +18,60 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //Other options are None, Native and Shadow DOM.
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy{
   //The curly brackets here are the definition in TypeScript of a JavaScript object.
   //So the property will only accept a JavaScript object.
   @Input('srvElement') element: { type: string, name: string, content: string };
+  //This property is a primitive, not reference object.
+  //(based according JavaScript types, visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures).
+  //We change the use of a reference to an object (like the element property above) to a primitive, so Angular can fire the ngOnChanges in this component.
+  //If we use a reference to the object and made a modification outside of this component (like in a parent component),
+  //the modification will be made, but Angular will not detect the change.
+  //Why? Because this property and the array element will have the same pointer in memory and it will not change the pointer like in a primitive change.
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(this.constructor.name + ' constructor called!');
   }
 
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    console.log(this.constructor.name + ' ngOnChanges called! with parameter changes object below');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log(this.constructor.name + ' ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log(this.constructor.name + ' ngDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.constructor.name + ' ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log(this.constructor.name + ' ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log(this.constructor.name + ' ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log(this.constructor.name + ' ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log(this.constructor.name + ' ngOnDestroy called!');
+  }
 }
